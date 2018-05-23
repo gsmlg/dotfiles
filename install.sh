5465e849a54869541ea8dc319deac43662e59036
gsmlg <gsmlg.com@gmail.com>
2018-05-23 21:37:57 +0800

    add vimrc and oh-my-zsh

--- install.sh
+++ install.sh
@@ -1,6 +1,14 @@
 #!/bin/bash
 
-ln -s ~/.dotfiles/emacs.d/init.el ~/.emacs
+# install emacs
+ln -f -s ~/.dotfiles/emacs.d/init.el ~/.emacs
 
+# install vim
+cat ~/.dotfiles/vimrc/vimrcs/basic.vim > ~/.vimrc
+# or
+#git clone --depth=1 https://github.com/amix/vimrc.git ~/.vim_runtime
+#sh ~/.vim_runtime/install_basic_vimrc.sh
 
-
+# install oh-my-zsh
+#sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+ln -f -s ~/.dotfiles/oh-my-zsh/zshrc ~/.zshrc

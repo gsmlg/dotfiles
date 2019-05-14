cb164521cc44dab10dda4642f7b1ef1af1308b58
gsmlg <me@gsmlg.org>
2019-05-14 09:25:33 +0800

    fix go

--- emacs.d/lisp/init-go.el
+++ emacs.d/lisp/init-go.el
@@ -1,5 +1,6 @@
 (use-package go-mode
   :mode ("\\.go\\'" . go-mode)
+  :hook (go-mode . (lambda () ((set (make-local-variable 'before-save-hook) '(gofmt-before-save)))))
   :ensure t)
 
 (provide 'init-go)

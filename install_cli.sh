7384349b93f7b05e723c5b79d6bde10ef2abadc3
Jonathan Gao <gsmlg.com@gmail.com>
2023-02-10 14:21:42 +0800

    fix: Fix auto download url of gsmlg-cli.

--- install_cli.sh
+++ install_cli.sh
@@ -7,8 +7,34 @@
 #FILE=gsmlg-cli_freebsd_amd64
 #FILE=gsmlg-cli_linux_amd64
 #FILE=gsmlg-cli_linux_arm64
-FILE=gsmlg-cli_mac_amd64
+#FILE=gsmlg-cli_mac_amd64
 #FILE=gsmlg-cli_mac_arm64
+
+SYS="$(uname -s).$(uname -p)"
+
+case $SYS in
+  Darwin.i386)
+  FILE=gsmlg-cli_mac_amd64
+  ;;
+  Darwin.arm)
+  FILE=gsmlg-cli_mac_arm64
+  ;;
+  Linux.x86_64)
+  FILE=gsmlg-cli_linux_amd64
+  ;;
+  Linux.aarch64)
+  FILE=gsmlg-cli_linux_arm64
+  ;;
+  FreeBSD.amd64)
+  FILE=gsmlg-cli_freebsd_amd64
+  ;;
+  *)
+  echo "Unsupported $SYS"
+  exit 1
+  ;;
+esac
+
+exit
 
 URL=$(curl -s https://api.github.com/repos/gsmlg-dev/gsmlg-cli/releases/latest |grep browser_download_url |grep $FILE |awk '{print $2}' | tr -d \")
 

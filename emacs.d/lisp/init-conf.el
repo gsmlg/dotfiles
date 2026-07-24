e1f535ecb93691c67f9694dcc02520fb3e83dd83
Jonathan Gao <gsmlg.com@gmail.com>
2026-07-24 14:12:52 +0800

    feat(emacs): add caddyfile-mode support to init-conf

--- emacs.d/lisp/init-conf.el
+++ emacs.d/lisp/init-conf.el
@@ -20,8 +20,11 @@
   :ensure t
   :config
   (eval-after-load 'nginx-mode
-    '(add-hook 'nginx-mode-hook #'company-nginx-keywords))
-  )
+    '(add-hook 'nginx-mode-hook #'company-nginx-keywords)))
+
+(use-package caddyfile-mode
+  :ensure t
+  :mode ("Caddyfile\\'" "\\.caddyfile\\'"))
 
 (provide 'init-conf)
 ;;; init-conf.el ends here
